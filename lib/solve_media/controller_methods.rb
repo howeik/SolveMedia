e98d33400727e45ee351833dfab0e79df9fec671
Tyler Cunnion <tyler@solvemedia.com>
2013-02-28 11:36:05 -0500

    Allow setting model errors

--- lib/solve_media/controller_methods.rb
+++ lib/solve_media/controller_methods.rb
@@ -1,7 +1,16 @@
 module SolveMedia
     module ControllerMethods
         def verify_solvemedia_puzzle(options={})
-            return SolveMedia.verify(params[:adcopy_challenge], params[:adcopy_response], VKEY, HKEY, request.remote_ip)
+            ver_options = { :validate_response => options[:validate_response] || true,
+                            :timeout           => options[:timeout] || 5
+            }
+            verified = SolveMedia.verify(params[:adcopy_challenge], params[:adcopy_response], VKEY, HKEY, request.remote_ip)
+            if options[:model] && !verified
+                options[:model].valid?
+                options[:model].errors.add(:base, options[:error_message] || "Please fill out the Solve Media puzzle")
+            end
+
+            return verified
         end
     end
 end
--- lib/solve_media/version.rb
+++ lib/solve_media/version.rb
@@ -1,3 +1,3 @@
 module SolveMedia
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
