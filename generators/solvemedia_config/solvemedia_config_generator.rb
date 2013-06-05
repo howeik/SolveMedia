541ebd074a8c31a554c0a3005c76e0054a1e51e5
Tyler Cunnion <tyler@solvemedia.com>
2013-06-05 09:44:48 -0400

    Prepare for new version

--- generators/solvemedia_config/solvemedia_config_generator.rb
+++
@@ -1,7 +0,0 @@
-class SolvemediaConfigGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.file "solvemedia_config.yml", "config/solvemedia_config.yml"
-    end
-  end
-end
--- install.rb
+++
@@ -1,4 +0,0 @@
-puts ""
-puts "Run 'script/generate solvemedia_config' to generate solvemedia_config.yml"
-puts "Don't forget to add your API keys to config/solvemedia_config.yml"
-puts ""
